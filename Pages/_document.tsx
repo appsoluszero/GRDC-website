3b80d1b9b7135daa79f020fc8880c72135c65568
Appy <6334424123@student.chula.ac.th>
2021-07-23 17:19:43 +0700

    :zap: UPDATE: Added a new font for Thai language.
    - Added 'Prompt' as a font for Thai language.
    - Reformatted the code using VSCode's Prettier extension
    - Slight changes to the header's texts

--- Pages/_document.tsx
+++ Pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
@@ -9,14 +9,18 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Roboto"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Prompt&family=Roboto"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
-import GRCHeader from '../modules/header/GRCHeader'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import GRCHeader from "../modules/header/GRCHeader";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <>
-    <GRCHeader/>
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <GRCHeader />
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
