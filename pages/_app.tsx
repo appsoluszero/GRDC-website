2aa330858151648bed8846e2686f26a80a9c50c5
thanadolps <thanadolps@gmail.com>
2021-07-23 23:35:15 +0700

    ✨ADD: next.js authentication system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import GRCHeader from "../modules/header/GRCHeader";
+import { Provider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider session={pageProps.session}>
       <GRCHeader />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 export default MyApp;
