04fde1743084099283700bc0b3422ca262d482da
thanadolps <thanadolps@gmail.com>
2021-08-07 06:11:20 +0700

    🗃️CHANGE: use postgresql instade of mongoDB
    # Difference
    Apart from difference in prisma schema,
    the id of mongodb has type of `ObjectId` (string) while the id of postgresql is `Int`
    
    # Rational
    Previous database use mongo atlast free tier which has reveal some disadvantage,
    - Unexpectedly slow on start up (approx 30 second)
    - Hard to install locally for testing
    - Prisma's support of it (NoSQL) is in beta
    As such, I made the decision to switch to postgresql on heroku, for now.

--- common/types/news.ts
+++ common/types/news.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import { assert, IsExact } from "conditional-type-checks";
 
 export const newsSchema = z.object({
-  id: z.string(),
+  id: z.number().positive(),
   title: z.string().min(1),
   content: z.string().min(1),
 });
