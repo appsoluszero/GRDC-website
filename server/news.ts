f07d93c354ee175220212c3bb45c3685e2f69922
thanadolps <thanadolps@gmail.com>
2021-08-08 23:24:31 +0700

    🩹FIX: catch and printout error in news's api

--- server/news.ts
+++ server/news.ts
@@ -5,24 +5,30 @@ import { createRouter } from "../pages/api/trpc/[trpc]";
 export default createRouter()
   .query("all", {
     async resolve({ ctx }) {
-      return await ctx.prisma.news.findMany();
+      return await ctx.prisma.news
+        .findMany()
+        .catch((err) => console.error(err));
     },
   })
   .mutation("post", {
     input: newsToUploadSchema,
     async resolve({ ctx, input: news }) {
-      await ctx.prisma.news.create({
-        data: news,
-      });
+      await ctx.prisma.news
+        .create({
+          data: news,
+        })
+        .catch((err) => console.error(err));
     },
   })
   .mutation("delete", {
     input: newsSchema.shape.id,
     async resolve({ ctx, input: id }) {
-      await ctx.prisma.news.delete({
-        where: {
-          id,
-        },
-      });
+      await ctx.prisma.news
+        .delete({
+          where: {
+            id,
+          },
+        })
+        .catch((err) => console.error(err));
     },
   });
